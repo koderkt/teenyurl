7a850937f15ae486cf150d4da3bb85499c58e0b4
koderkt <chetham1999renuka@gmail.com>
2024-07-09 21:38:53 +0530

    static home page

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -4,26 +4,44 @@
 		<!-- Logo -->
 		<div class="flex justify-between">
 			<div class="font-sans text-2xl font-[750] p-2">TEENYURL</div>
-			<div class="flex font-bold">
+			<div class="flex item-center font-bold">
 				<!-- LOGIN BUTTON -->
-				<div class="p-2">
-					<button>Login</button>
+				<div class="py-4">
+					<button class="p-2 px-4 text-gray-700">Login</button>
 				</div>
 				<!-- LOGIN BUTTON -->
-				<div class="p-2">
-					<button>Sign Up</button>
+				<div class=" py-4">
+					<button class="p-2 px-4 bg-cyan-400 rounded-3xl text-white">Sign Up</button>
 				</div>
 			</div>
 		</div>
 		<!-- Parent container for body -->
-		<div class="flex py-60">
-			<div class="">
-				Paste your log URL here
-				<input value="Enter you link here...." />
-				<input readonly value="your short link" />
+		<div class="flex flex-col justify-between items-center py-20 md:flex-row">
+			<div class="p-28 px-10">
+				<span class="font-sans text-2xl font-semibold"> Paste your log URL here:</span>
+
+				<div class="flex flex-col">
+					<div class="flex items-center border-2 border-grey-800 rounded-3xl mt-8">
+						<!-- <div></div> -->
+						<input class="outline-none flex-grow p-2 rounded-3xl" value="Enter you link here...." />
+						<div>
+							<button class="px-8 py-2 text-white font-sans font-bold bg-cyan-400 rounded-3xl"
+								>Shorten</button
+							>
+						</div>
+					</div>
+				</div>
+				<div class="flex items-center mt-8">
+					<div class="ml-2">Your Short URL:</div>
+					<div class="flex items-center border-2 ml-4 border-grey-800 rounded-3xl">
+						<input class="ml-3 p-1 outline-none" value="your short link" />
+						<button class="px-6 p-2 rounded-3xl bg-gray-500 font-bold text-white">Copy</button>
+					</div>
+				</div>
 			</div>
-			<div>
-				<img src="src/assets/images/illustration-working.svg" alt="" />
+
+			<div class="px-5">
+				<img class="" src="src/assets/images/illustration-working.svg" alt="" />
 			</div>
 		</div>
 	</div>
